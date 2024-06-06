11ac0ecc054ffc21faa7081031773c1cf2f6fed5
Vaiditya2207 <vaidityatanwar2207@gmail.com>
2024-06-06 14:52:43 +0530

    configured a basic paging layout with some pages having navbar and some pages not having navbar

---
+++ app/(without-navbar)/layout.tsx
@@ -0,0 +1,17 @@
+export const metadata = {
+    title: 'CodeMelon',
+    description: 'CodeMelon is a website where you can find various tools to help you with your coding journey.',
+  }
+  
+  export default function RootLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    )
+  }
+  
